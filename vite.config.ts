932f30b1a78ff73b1c662e3117ed0c8c6d61b9c8
WasabiThumb <wasabithumbs@gmail.com>
2024-09-30 12:57:48 -0400

    Some improvements
    https://github.com/nodejs/readable-stream/issues/539#issuecomment-2383555497

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import {defineConfig} from "vite";
 import {nodePolyfills} from "vite-plugin-node-polyfills";
 
+// Receives BUILD_TARGET environment variable, set by package.json
+const kTarget: string = "BUILD_TARGET";
+let target: string = (kTarget in process.env && ((env) => {
+    let val: any = env[kTarget];
+    if (typeof val === "string") return val;
+    return false;
+})(process.env)) || "UNKNOWN";
+
+// Standard Vite config
 export default defineConfig({
     mode: 'development',
-    base: '/reprex-539',
+    base: target === 'PAGES' ? '/reprex-539' : '/',
     build: {
         sourcemap: true
     },
